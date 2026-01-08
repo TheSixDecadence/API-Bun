efc70f436ede5b08bd603fd10a9418c4defe41e9
Jesús De los Santos <DelosSantosJ.10.3A@gmail.com>
2026-01-07 20:10:45 -0600

    Se agrega serviciode groq y cerebras

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,40 @@
+import { cerebrasService } from "../services/cerebras";
+import { groqService } from "../services/groq";
+import type { AIService, ChatMessage } from "../services/types";
+
+const services: AIService[] = [
+    groqService,
+    cerebrasService
+]
+let currentServiceIndex = 0;
+
+function getNextService() {
+    const service = services[currentServiceIndex];
+    currentServiceIndex = (currentServiceIndex + 1) % services.length;
+    return service;
+}
+
 export default {
     async fetch(req: Request) {
-        return new Response("Bun chambeanding");
+        const { pathname } = new URL(req.url);
+
+        if (req.method === 'POST' && pathname === '/chat') {
+            const { messages } = await req.json() as { messages: ChatMessage[] };
+            const service = getNextService();
+            
+            console.log(`Using service: ${service?.name}`);
+
+            const stream = await service?.chat(messages);
+
+            return new Response(stream, {
+                headers: {
+                    'Content-Type': 'text/event-stream',
+                    'Cache-Control': 'no-cache',
+                    'Connection': 'keep-alive',
+                },
+            });
+        }
+        return new Response('not found', { status: 404 })
     } 
 }
 
